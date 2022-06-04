15ba3be4ba7e2c5e4dece834982fde019978bce5
Mugen <paradigmcomplex@gmail.com>
2022-06-04 13:29:33 -0500

    Base view for facial recognition website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import Navigation from "./components/navigation/Navigation";
+import Logo from "./components/logo/Logo";
+import ImageLinkForm from "./components/imagelinkform/ImageLinkForm";
+import Rank from "./components/rank/Rank";
+import { particleOptions } from "./particleOptions";
+import "tachyons";
 
-function App() {
+const App = () => {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particleOptions}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">{'Mugen, your current rank is 1'}</div>
+    </div>
+  );
+};
+
+export default Rank;
